8efb802cac613ab8cb94fe008a351862d78d3d49
asmaaiouez1 <asma@puentedev.io>
2019-04-20 16:36:51 +0300

    updateing

--- app/src/main/java/com/example/mysql_test/MainActivity.java
+++ app/src/main/java/com/example/mysql_test/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.mysql_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.mysql_test.helper.CheckNetworkStatus;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button viewAllBtn = (Button) findViewById(R.id.viewAllBtn);
+        Button addNewBtn = (Button) findViewById(R.id.addNewBtn);
+        viewAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                Toast.makeText(MainActivity.this,
+                        "You clicked on View All",
+                        Toast.LENGTH_LONG).show();
+                if (CheckNetworkStatus.isNetworkAvailable(getApplicationContext())) {
+                    Toast.makeText(MainActivity.this,
+                            "Inside IF",
+                            Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(getApplicationContext(),
+                            UserListingActivity.class);
+                    startActivity(i);
+                } else {
+                    //Display error message if not connected to internet
+                    Toast.makeText(MainActivity.this,
+                            "Unable to connect to internet",
+                            Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
+        addNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Check for network connectivity
+                if (CheckNetworkStatus.isNetworkAvailable(getApplicationContext())) {
+                    Intent i = new Intent(getApplicationContext(),
+                            AddUserActivity.class);
+                    startActivity(i);
+                } else {
+                    //Display error message if not connected to internet
+                    Toast.makeText(MainActivity.this,
+                            "Unable to connect to internet",
+                            Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
     }
 }
